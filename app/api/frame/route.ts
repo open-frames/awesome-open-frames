404f27fe42e338320912a7b6c72737c00e6c8c06
Sanjay Raveendran <sanjayprabhu@gmail.com>
2024-01-30 13:57:30 -0800

    Fix status code

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  return NextResponse.redirect('https://redirect-frame.vercel.app/redirect');
+  return NextResponse.redirect('https://redirect-frame.vercel.app/redirect', {status: 302});
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
