0508922c1d2eb37ec82ea071127ed969e5ef6031
fabri <fguespe@gmail.com>
2024-03-21 10:20:14 -0300

    update

--- app/api/prs/route.ts
+++ app/api/prs/route.ts
@@ -4,7 +4,23 @@ import { NEXT_PUBLIC_URL } from '../../config';
 import sharp from 'sharp';
 import fs from 'fs';
 import path from 'path';
-import satori from 'satori';
+
+const fetchOpenPRCount = async () => {
+  const repo = 'open-frames/awesome-open-frames';
+  const url = `https://api.github.com/repos/${repo}/pulls?state=open`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(`Number of open PRs: ${data.length}`);
+    return data.length;
+  } catch (error) {
+    console.error('Error fetching open PR count:', error);
+  }
+};
 
 async function generateAndSavePNG(svgContent: string): Promise<string> {
   const pngBuffer = await sharp(Buffer.from(svgContent)).png().toBuffer();
@@ -15,25 +31,14 @@ async function generateAndSavePNG(svgContent: string): Promise<string> {
 }
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  // HTML content as a string
-  const htmlContent = `
-  <div>Hello, World</div>
+  const openPRCount = await fetchOpenPRCount();
+
+  const svgContent = `
+  <svg width="500" height="250" xmlns="http://www.w3.org/2000/svg">
+    <rect width="100%" height="100%" fill="white"/>
+    <text x="50%" y="50%" font-family="Arial" font-size="30" fill="black" text-anchor="middle" dominant-baseline="middle">${openPRCount} Frames submitted!</text>
+  </svg>
 `;
-
-  const inter = fs.readFileSync(path.join(process.cwd(), 'public', 'fonts', 'Inter-Regular.ttf'));
-
-  const svgContent = await satori('<div style="color: red;">hello, world</div>', {
-    width: 600,
-    height: 400,
-    fonts: [
-      {
-        name: 'Inter',
-        data: inter,
-        weight: 400,
-        style: 'normal',
-      },
-    ],
-  });
 
   console.log(svgContent);
   const imageUrl = await generateAndSavePNG(svgContent);
@@ -42,11 +47,11 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     getFrameHtmlResponse({
       buttons: [
         {
-          label: `We love BOAT`,
+          label: `Back to the main page`,
         },
       ],
       image: `${NEXT_PUBLIC_URL}/${imageUrl}`,
-      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+      postUrl: `${NEXT_PUBLIC_URL}/`,
     }),
   );
 }
